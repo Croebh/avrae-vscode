abff707ce23fb2f2d298f4161eb8227bcb6b624d
Croebh <icrona@gmail.com>
2021-12-19 15:35:08 -0700

    Get Update Gvar to workish, edit readme

--- README.md
+++ README.md
@@ -1,65 +1,28 @@
-# avrae-test README
+# Avrae Utilities for SublimeText
+A plugin for VSCode containing utilities for the [Avrae](https://avrae.io) Discord bot.
 
-This is the README for your extension "avrae-test". After writing up a brief description, we recommend including the following sections.
+## Important Note
+This package makes a remote connection to the Avrae API in order to collect and update the information. It only does this by request, and will not make any outward connection without being prompted to by the user.
+
+## Setup
+In order for this plugin to have your permissions to grab and update your GVARs, Workshop Aliases, or Workshop Snippets, you need to give it your token.
+
+1. Go to [Avrae](https://avrae.io) and log in to the dashboard
+2. Press F12 to open the DevTools
+3. Go to the 'Application' tab
+4. On the left, select 'https://avrae.io' under 'Local Storage'
+5. Copy the 'Value' next to the 'avrae-token' key
+6. In VSCode, go to File -> Preferences -> Settings 
+7. Under Extensions, select Avrae Utilities pasting the copied token into the "Avrae: Token" setting
+
+### Note
+Please keep this token private, as anyone who gains access to this token could potentially gain access to your Discord account.
 
 ## Features
+This plugin contains the following features:
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Draconic Syntax
+Handy syntax highlighting for the Draconic language (Subset of Python) that Avrae uses for aliasing. Its set up to work automatically on `*.alias`, `*.snippet`, and `*.gvar` filetypes.
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+### Get and Update GVARs
+Using the ``Avrae Utilities: Get GVAR`` and ``Avrae Utilities: Update GVAR`` commands in the Command Pallete, you can retrieve and update GVARs without the need to visit the dashboard. If you want to save them locally, you can use the file extension ``.gvar``, and include the ID for the GVAR anywhere in the rest of the file name. You can have labels/descriptions before or after the ID, allowing you to organize your GVARs.
--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 const vscode = require('vscode');
 const axios = require('axios');
-
+const path = require("path");
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+	const uuid_pattern = /[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}/ig;
+
 	const token = vscode.workspace.getConfiguration('avrae').get('token');
 	const instance = await axios.create({
 		baseURL: "https://api.avrae.io/",
@@ -19,31 +21,106 @@ async function activate(context) {
 		}
 	});
 	
-	let disposable = vscode.commands.registerCommand('avrae-test.getGvar', async function () {
-		// console.log(vscode.workspace.getConfiguration('avrae').get('token'));
-		// vscode.window.showInformationMessage('Hello World from avrae-test!');
-		const gvarID = await vscode.window.showInputBox({
-			ignoreFocusOut: true,
-			title: "What GVAR would you like to get?"
-		});
-		const result = await instance.get("/customizations/gvars/" + gvarID);
-		const editor = vscode.window.activeTextEditor;
+	let getGvar = vscode.commands.registerCommand('avrae-utilities.getGvar', async function () {
 
-		if (editor) {
-			editor.edit(editBuilder => {
-				editBuilder.insert(editor.selection.active, result.data.value);
+		// Check if file name == gvarID + '.gvar otherwise get
+		const editor = vscode.window.activeTextEditor;
+		var gvarID = "";
+		var filePath = editor.document.fileName;
+		var fileName = path.basename(filePath);
+		
+		if (editor && fileName.match(uuid_pattern)) {
+			gvarID = fileName.match(uuid_pattern)[0]
+		};
+
+		if (!gvarID) {
+			gvarID = await vscode.window.showInputBox({
+				ignoreFocusOut: true,
+				title: "What GVAR would you like to get?"
 			});
-		} else {
-			vscode.workspace.openTextDocument(
-				{
-					content: result.data.value, 
-					language: "json"
-				}
-			);
 		}
+
+		if (gvarID && uuid_pattern.test(gvarID)) {
+			const result = await instance.get("/customizations/gvars/" + gvarID);
+			
+			if (result.status != 200 ) {
+				vscode.window.showInformationMessage("Error!");
+			}
+
+			// Check if file name == gvarID + '.gvar'
+			// If so, delete the file, replace with new contents
+			// Otherwise, new doc
+			if (editor && fileName.match(uuid_pattern)) {
+				await editor.edit(editBuilder => {
+					var firstLine = editor.document.lineAt(0);
+					var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+					var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+					editBuilder.delete(textRange);
+				});
+				await editor.edit(editBuilder => {
+					editBuilder.insert(new vscode.Position(0, 0), result.data.value);
+				});
+			} else {
+				vscode.workspace.openTextDocument(
+					{
+						content: result.data.value, 
+						language: "json"
+					}
+				);
+			}
+			vscode.window.showInformationMessage("Gvar ID: " + gvarID + "\nSuccessfully Grabbed");
+		}
+
+		
+
+		
 	});
 
-	context.subscriptions.push(disposable);
+	let updateGvar = vscode.commands.registerCommand('avrae-utilities.updateGvar', async function () {
+
+		// Check if file name == gvarID + '.gvar otherwise get
+		const editor = vscode.window.activeTextEditor;
+		var gvarID = "";
+		var filePath = editor.document.fileName;
+		var fileName = path.basename(filePath);
+		
+		var firstLine = editor.document.lineAt(0);
+		var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+		var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+		var newData = editor.document.getText(textRange);
+
+		if (newData.length > 100000) {
+			vscode.window.showInformationMessage("Gvars are limited to 100k characters.")
+			return
+		}
+
+		if (editor && fileName.match(uuid_pattern)) {
+			gvarID = fileName.match(uuid_pattern)[0]
+
+			const getResult = await instance.get("/customizations/gvars/" + gvarID);
+
+			if (getResult.status != 200 ) {
+				vscode.window.showInformationMessage("Error!");
+			}
+
+
+			var payload = getResult.data
+			payload.value = newData
+			const postResult = await instance.post("/customizations/gvars/" + gvarID, payload)
+			if (postResult.status === 200 ) {
+				vscode.window.showInformationMessage("Gvar ID: " + gvarID + "\nSuccessfully Updated");
+			} else {
+				vscode.window.showInformationMessage("Error!");
+			}
+		};
+	});
+
+	// TODO
+
+	// Publish v0.0.1? haha
+
+	context.subscriptions.push(getGvar);
+	context.subscriptions.push(updateGvar);
 }
 
 function deactivate() {}
